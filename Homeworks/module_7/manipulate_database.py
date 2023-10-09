240e905bdcc49c9443799c22e5a0181ec560dc5a
Olga_Semakina <olga_semakina@epam.com>
2023-10-09 23:15:08 +0300

    add homework for module #7

---
+++ Homeworks/module_7/manipulate_database.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from setup_database import Film
+from setup_database import db_name
+
+engine = create_engine("sqlite:///" + db_name, echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# add 3 films to the table
+films = [Film(title='The Lord of the Rings', director='Peter Jackson', release_year='2003'),
+         Film(title='The Dark Knight', director='Christopher Nolan', release_year='2008'),
+         Film(title='Pulp Fiction', director='Quentin Tarantino', release_year='1990')]
+for film in films:
+    session.add(film)
+session.commit()
+
+# update the entry in the table
+films[2].release_year = '1994'
+session.commit()
+
+# print data
+inserted_films = session.query(Film).all()
+for film in inserted_films:
+    print(film.id, film.title, film.director, film.release_year)
+
+# delete data
+for film in inserted_films:
+    session.delete(film)
+session.commit()
+
+# close session
+session.close()
