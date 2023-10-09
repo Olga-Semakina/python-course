b9cd2febfc03c8b71f698e3d82853481a733d41e
Olga_Semakina <olga_semakina@epam.com>
2023-10-09 23:15:08 +0300

    add homework for module #7

---
+++ Homeworks/module_7/setup_database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+
+db_name = 'films_db'
+engine = create_engine("sqlite:///" + db_name, echo=True)
+
+Base = declarative_base()
+
+
+class Film(Base):
+    __tablename__ = "films"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    director = Column(String)
+    release_year = Column(Integer)
+
+
+Base.metadata.create_all(engine)
