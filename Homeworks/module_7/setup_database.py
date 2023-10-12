971cd846771bc6c2898c8e3da3cbc3cc21c86324
Olga_Semakina <olga_semakina@epam.com>
2023-10-12 17:45:04 +0300

    fix for hw#7

--- Homeworks/module_7/setup_database.py
+++ Homeworks/module_7/setup_database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 
 db_name = 'films_db'
-engine = create_engine("sqlite:///" + db_name, echo=True)
+engine = create_engine("sqlite:///{}.db".format(db_name), echo=True)
 
 Base = declarative_base()
 
